abc0b1fc7ce25a3e2934da98eb9c4bf6fe797512
kstasi <nastasiakondaurova@gmail.com>
2020-12-18 17:06:59 +0200

    migrations  added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,14 +2,9 @@ const Multisig = artifacts.require("Multisig");
 const accounts = require("../scripts/sandbox/accounts");
 module.exports = async (deployer, _network, _accounts) => {
   deployer.deploy(Multisig, {
-    current_proposal: null,
-    threshold: 2,
-    approved_by: [],
-    owners: [accounts.alice.pkh, accounts.bob.pkh, accounts.eve.pkh],
+    proposal: null,
+    required: 2,
+    approved: [],
+    keys_set: [accounts.alice.pkh, accounts.bob.pkh, accounts.eve.pkh],
   });
-  //   deployer.deploy(Multisig, {
-  //     stored_counter: 0,
-  //     threshold: 2,
-  //     keys: [accounts.alice.pk, accounts.bob.pk, accounts.eve.pk],
-  //   });
 };
